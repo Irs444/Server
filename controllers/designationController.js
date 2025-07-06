3bf7098d787e79460163803cffa28581e8ae398c
Irs444 <irs786had123@gmail.com>
2025-07-06 20:52:36 +0530

    designation update and delete controller and router created

--- controllers/designationController.js
+++ controllers/designationController.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Designation = require("../models/designationModal");
+const User = require("../models/userModal");
 
 const addDesignation = async (req, res) => {
     try {
@@ -78,4 +79,37 @@ const getAllDesignation = async (req, res) => {
     }
 }
 
-module.exports = { addDesignation, getAllDesignation }
+const updateDesignation = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, departmentId } = req.body
+        const currentUser = req.user
+        if (currentUser.role !== "admin") return res.status(403).send({ message: 'Access denied. Only admin can update designation' })
+        if (name == '' || departmentId == '') return res.status(400).send({ message: 'Fields cannot be empty' })
+        const designation = await Designation.findOne({ name, departmentId })
+        if (designation) return res.status(400).send({ message: 'Designation already exist' })
+        const updatedDesignation = await Designation.updateOne({ _id: id }, { $set: { name, departmentId } })
+        if (updatedDesignation.acknowledged !== true) return res.status(404).send({ message: 'Designation not found' })
+        return res.status(200).send({ message: 'Designation update successfully' })
+    } catch (err) {
+        return res.status(500).send({ message: err.message })
+    }
+}
+
+const deleteDesignation = async (req, res) => {
+    try {
+        const { id } = req.params
+        const currentUser = req.user
+        if (currentUser.role !== 'admin') return res.status(403).send({ message: 'Access denied. Only admin can delete designation' })
+        const designation = await Designation.findById(id)
+        if (!designation) return res.status(404).send({ message: 'Designation not found' })
+        const employee = await User.find({ designation: id })
+        if (employee.length > 0) return res.status(400).send({ message: 'Access denied. Employee assign to this designation' })
+        await Designation.findByIdAndDelete(id)
+        return res.status(200).send({ message: 'Designation delete successfully' })
+    } catch (err) {
+        return res.status(500).send({ message: err.message })
+    }
+}
+
+module.exports = { addDesignation, getAllDesignation, updateDesignation, deleteDesignation }
--- routers/designationRouter.js
+++ routers/designationRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { authMiddleware } = require('../middlewares/multerMiddleware');
-const { addDesignation, getAllDesignation } = require('../controllers/designationController');
+const { addDesignation, getAllDesignation, updateDesignation, deleteDesignation } = require('../controllers/designationController');
 const router = express.Router();
 
 router.post("/add", authMiddleware, addDesignation)
 router.get("/list", authMiddleware, getAllDesignation)
+router.put("/:id/update", authMiddleware, updateDesignation)
+router.delete("/:id", authMiddleware, deleteDesignation)
 
 module.exports = router
